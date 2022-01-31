2959df6d7c4b1874e30fe007aa5f028945a8cc17
Léderer András <andrew@file.glass>
2022-01-31 23:09:22 +0100

    chore: create exports

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,13 @@
-import {test} from "./test";
 const cwd = process.cwd()
 export default cwd
 export const isDev = !!process.env.DEV
-test()
+
+export {useDumbEvent, useNetEvent} from "./events"
+export {createFiber, useFiber} from "./fiber"
+export {useServerState, useStateEffect} from "./state"
+export {useVector3, useVector2, useVectorialDistance} from "./vectors"
+declare function debug(...msg: unknown[]): void
+declare function trace(...msg: unknown[]): void
+declare function info(...msg: unknown[]): void
+declare function silly(...msg: unknown[]): void
+declare function error(...msg: unknown[]): void
